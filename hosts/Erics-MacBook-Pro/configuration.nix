67dabcf0f2cef7142f4d00d39d048d5aa8fca882
James Danylik <james.danylik@gmail.com>
2023-11-17 02:31:45 -0800

    Fix font issue on Darwin

--- hosts/Erics-MacBook-Pro/configuration.nix
+++ hosts/Erics-MacBook-Pro/configuration.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ pkgs, ... }: {
   # system.stateVersion = "23.05";
   nixpkgs.hostPlatform = "aarch64-darwin";
   nixpkgs.config.allowUnfree = true;
@@ -8,6 +8,14 @@
   system.defaults.dock.autohide = true;
 
   programs.zsh.enable = true;
+
+  fonts = {
+    fontDir.enable = true;
+    fonts = with pkgs; [
+      (nerdfonts.override { fonts = [ "Noto" ]; })
+      twemoji-color-font # for monochrome emjoi fallbacks
+    ];
+  };
 
   users.users.ericngo = {
     name = "ericngo";
